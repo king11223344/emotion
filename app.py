298ec2d49cb09a61ef418e9ba71dcf784da6c27d
king11223344 <51441410+king11223344@users.noreply.github.com>
2023-01-25 03:37:28 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,66 @@
+
+from flask import Flask, request, jsonify, send_file
+import cv2
+from PIL import Image, ImageTk
+import os
+import numpy as np
+import urllib.request
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D
+from keras.optimizers import Adam
+from keras.layers import MaxPooling2D
+from keras.preprocessing.image import ImageDataGenerator
+
+app = Flask(__name__)
+emotion_model = Sequential()
+
+emotion_model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
+emotion_model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Dropout(0.25))
+
+emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Dropout(0.25))
+
+emotion_model.add(Flatten())
+emotion_model.add(Dense(1024, activation='relu'))
+emotion_model.add(Dropout(0.5))
+emotion_model.add(Dense(7, activation='softmax'))
+emotion_model.load_weights('./static/emotion_model.h5')
+
+cv2.ocl.setUseOpenCL(False)
+
+emotion_dict = {0: "   Angry   ", 1: "Disgusted", 2: "  Fearful  ", 3: "   Happy   ", 4: "  Neutral  ", 5: "    Sad    ", 6: "Surprised",7:"NO Face"}
+
+
+
+#creating route for get request
+@app.route('/emotion', methods=['GET'])
+def getmood():
+    param = request.args.get('param')
+    urllib.request.urlretrieve(param, "abc.png")
+    img = Image.open("abc.png")
+    numpydata=np.asarray(img)
+    predictions=[]
+    frame1=numpydata
+    frame1 = cv2.resize(frame1,(600,500))
+    bounding_box = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    gray_frame = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+    num_faces = bounding_box.detectMultiScale(gray_frame,scaleFactor=1.3, minNeighbors=5)
+    maxindex=7
+    for (x, y, w, h) in num_faces:
+        cv2.rectangle(frame1, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
+        roi_gray_frame = gray_frame[y:y + h, x:x + w]
+        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray_frame, (48, 48)), -1), 0)
+        prediction = emotion_model.predict(cropped_img)
+        maxindex = int(np.argmax(prediction))
+        predictions.append(emotion_dict[maxindex])
+    return emotion_dict[maxindex]
+app.run(host='0.0.0.0')
+
+
+        
